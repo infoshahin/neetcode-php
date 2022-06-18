e7b64a20589d9d36e6c73377baeaa442ca8fe687
Nuruzzaman Milon <contact@milon.im>
2022-06-18 16:03:59 +0200

    added O(1) solution for min-stack

--- stack/min-stack.php
+++ stack/min-stack.php
@@ -2,7 +2,8 @@
 
 // URL: https://leetcode.com/problems/min-stack/
 
-class MinStack
+// getMin has O(n) time complexity
+class MinStackUsingOneStack
 {
     private array $stack;
 
@@ -37,6 +38,67 @@ class MinStack
         return min($this->stack);
     }
 }
+
+// getMin has O(1) time complexity
+class MinStackUsingTwoStack
+{
+    private array $stack;
+    private array $minStack;
+
+    function __construct()
+    {
+        $this->stack = [];
+        $this->minStack = [];
+    }
+
+    /**
+     * @return NULL
+     */
+    function push(int $val)
+    {
+        $this->stack[] = $val;
+
+        if (count($this->minStack) === 0) {
+            $this->minStack[] = $val;
+            return;
+        }
+
+        $topOfMinStack = $this->minStack[array_key_last($this->minStack)];
+        if ($topOfMinStack < $val) {
+            $this->minStack[] = $topOfMinStack;
+        } else {
+            $this->minStack[] = $val;
+        }
+    }
+
+    /**
+     * @return NULL
+     */
+    function pop()
+    {
+        array_pop($this->stack);
+        array_pop($this->minStack);
+    }
+
+    function top(): int
+    {
+        return $this->stack[array_key_last($this->stack)];
+    }
+
+    function getMin(): int
+    {
+        return $this->minStack[array_key_last($this->minStack)];
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * $obj = MinStack();
+ * $obj->push($val);
+ * $obj->pop();
+ * $ret_3 = $obj->top();
+ * $ret_4 = $obj->getMin();
+ */
 
 /*
  * Your MinStack object will be instantiated and called as such:
