0808f21e7f8867d81ef5a350e82bce07196a3df5
Nuruzzaman Milon <contact@milon.im>
2022-06-26 14:53:04 +0200

    added course-schedule-ii

--- 11. graphs/course-schedule-ii.php
+++ 11. graphs/course-schedule-ii.php
@@ -1,18 +1,20 @@
 <?php
 
-// URL: https://leetcode.com/problems/course-schedule/
+// URL: https://leetcode.com/problems/course-schedule-ii/
 
 class Solution
 {
+    private array $result = [];
     private array $visit = [];
+    private array $cycle = [];
     private array $preMap = [];
 
     /**
      * @param int $numCourses
      * @param int[][] $prerequisites
-     * @return bool
+     * @return int[]
      */
-    function canFinish(int $numCourses, array $prerequisites): bool
+    function findOrder(int $numCourses, array $prerequisites): array
     {
         foreach ($prerequisites as $prerequisite) {
             [$course, $pre] = $prerequisite;
@@ -21,30 +23,33 @@ class Solution
 
         for ($crs = 0; $crs < $numCourses; $crs++) {
             if (!$this->dfs($crs)) {
-                return false;
+                return [];
             }
         }
-        return true;
+        
+        return $this->result;
     }
 
     function dfs(int $course): bool
     {
-        if (in_array($course, $this->visit)) {
+        if (in_array($course, $this->cycle)) {
             return false;
         }
 
-        if (empty($this->preMap[$course])) {
+        if (in_array($course, $this->visit)) {
             return true;
         }
 
-        array_push($this->visit, $course);
+        array_push($this->cycle, $course);
         foreach ($this->preMap[$course] as $crs) {
             if (!$this->dfs($crs)) {
                 return false;
             }
         }
-        array_pop($this->visit);
-        $this->preMap[$course] = [];
+        array_pop($this->cycle);
+        array_push($this->visit, $course);
+        
+        $this->result[] = $course;
 
         return true;
     }
